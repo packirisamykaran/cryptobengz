c983d786766241c16641dd41112ded4dae4adbc3
Karan <packirisamykaran@gmail.com>
2022-10-15 16:39:06 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 
 function App() {
     return (
-        <BrowserRouter basename="cbv2">
+        <BrowserRouter basename="/cbv2">
             <div className="app">
                 <Header />
                 <Routes>
